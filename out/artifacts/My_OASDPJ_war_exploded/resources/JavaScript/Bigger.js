b12ffc5ce472c2a2e1f1479a2f9480eb2127ca62
2449278392@qq.com <55231475+yoyazhang@users.noreply.github.com>
2020-07-30 23:12:56 +0800

    update css

--- out/artifacts/My_OASDPJ_war_exploded/resources/JavaScript/Bigger.js
+++ out/artifacts/My_OASDPJ_war_exploded/resources/JavaScript/Bigger.js
@@ -18,23 +18,17 @@ window.onload = function () {
         floatBox.style.display = "none";
         bigBox.style.display = "none";
     };
-    //鼠标移动事件
     smallBox.onmousemove = function (e) {
         let left = e.pageX - smallBox.offsetLeft - floatBox.offsetWidth / 2;
         let top = e.pageY - smallBox.offsetTop - floatBox.offsetHeight / 2;
-
         left = Math.max(left, 0);
         left = Math.min(left, smallBox.offsetWidth - floatBox.offsetWidth);
-
         top = Math.max(top, 0);
         top = Math.min(top, smallBox.offsetHeight - floatBox.offsetHeight);
-        //实现放大镜随着鼠标移动
         floatBox.style.left = left + "px";
         floatBox.style.top = top + "px";
-        //现有位置与可能达到的最大位置之比(可理解为位置移动了百分之多少)
         let percentX = left / (smallBoxImage.offsetWidth - floatBox.offsetWidth);
         let percentY = top / (smallBoxImage.offsetHeight - floatBox.offsetHeight);
-        //用相同的比例*可能的最大位置，由于放大镜实现原理，反向变动
         bigBoxImage.style.left = -percentX * (bigBoxImage.offsetWidth - bigBox.offsetWidth) + "px";
         bigBoxImage.style.top = -percentY * (bigBoxImage.offsetHeight - bigBox.offsetHeight) + "px";
     }
